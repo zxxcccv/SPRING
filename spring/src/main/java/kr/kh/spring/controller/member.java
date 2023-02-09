7748635bbacf5a467ab79a3dbe5d74c9cc0b6cd0
TH <zzxc6171@naver.com>
2023-02-09 21:19:51 +0900

    수정

---
+++ spring/src/main/java/kr/kh/spring/controller/member.java
@@ -0,0 +1,25 @@
+package kr.kh.spring.controller;
+
+public class member {
+	private String id;
+	private String pw;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	@Override
+	public String toString() {
+		return "member [id=" + id + ", pw=" + pw + "]";
+	}
+	
+	
+}
